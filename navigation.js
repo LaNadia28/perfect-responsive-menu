68160fdcc199a47b8b090946e3c88f1fad67e327
LAZUKINA <nadejda.cefim@gmail.com>
2020-03-30 17:57:34 +0200

    update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,6 +1,11 @@
 const toggleMenu = document.querySelector('.navigation button');
 const menu = document.querySelector('.navigation ul');
-const contacts = document.scrollingElement('li'); 
+const contacts = document.scrollingElement('li'); // Ctéation d'un élément li
+
+contacts.id = 'contacts'; // Définition de son identifiant
+contacts.textContent = 'Contacts'; // Définition de son contenue textuel
+
+document.getElementById('menu').appendChild(contacts); //Insertion du nouvel élément
 
 toggleMenu.addEventListener('click', function() {
 	const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
